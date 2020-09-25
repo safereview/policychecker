39f71d7a887eea3995f058cf131056a43a97a755
Hammad Afzali <hammad.afzali@gmail.com>
2020-09-25 17:47:50 -0400

    Add a brief description of all steps

---
+++ src/policy_checker.py
@@ -0,0 +1,23 @@
+# TODO: Extract the code review policy (CRP)
+
+# TODO: Extract and verify the CRP's signature
+
+# TODO: Parse and Interpret the CRP
+
+# TODO: Extract review units (RUs) from the repository
+
+# TODO: Validate RUs
+#   - Valid commit signature
+#   - Valid RU signature
+#   - Valid chain of reviews
+
+# TODO: Basic checks for
+#   - First commit
+#   - Direct push
+#   - Authorized merger
+#   - Minimum number of approving reviews
+#   - Required reviews from specific users
+#   - Stale approving reviews are dismissed
+#   - etc.
+
+# TODO: Check code reviews against review policies
