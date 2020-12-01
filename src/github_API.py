e159082106daba68ae516495924600b8cc50c3cc
Andres Orbe <aeo34@njit.edu>
2020-12-01 18:09:15 -0500

    Catch exceptions in branch protection calls #13

--- src/github_API.py
+++ src/github_API.py
@@ -10,8 +10,10 @@ from constants import *
 
 # Form a get request
 def get_request(endpoint):
-	return requests.get(f"{GITHUB_API}/{endpoint}", headers = HEADERS)
-
+	try:
+		return requests.get(f"{GITHUB_API}/{endpoint}", headers = HEADERS)
+	except Exception:
+		return False
 
 # Get info about one branch
 def get_branch(g, user, repo, branch):
@@ -85,58 +87,59 @@ def form_crp(g, user, repo, branch_name):
 
 # Get branch protection rules
 def get_branch_protection_rules(g, user, repo, branch_name):
-	#FIXME: What if the user pass a Branch which is not protected
-
 	endpoint = f"{user}/{repo}/branches/{branch_name}/protection"
 	rules = get_request(endpoint)
 
-	#TODO: DOC
-	result = dict()
-	if(rules.ok):
-		rules_json = json.loads(rules.content)
-		result['require_linear_history'] = rules_json["required_linear_history"]['enabled']
-		result['allow_force_pushes'] = rules_json["allow_force_pushes"]['enabled']
-		result['allow_deletions'] = rules_json["allow_deletions"]['enabled']
+	try:
+		#TODO: DOC
+		result = dict()
+		if(rules.ok):
+			rules_json = json.loads(rules.content)
+			result['require_linear_history'] = rules_json["required_linear_history"]['enabled']
+			result['allow_force_pushes'] = rules_json["allow_force_pushes"]['enabled']
+			result['allow_deletions'] = rules_json["allow_deletions"]['enabled']
 
-		#TODO: We try the rules_dump becasue
-		try:
-			rules_dump = json.dumps(rules.json())
-			dismiss_stale = bool(re.search('"dismiss_stale_reviews": true', rules_dump))
-			require_codeowners = bool(re.search('"require_code_owner_reviews": true', rules_dump))
-			# get the minimum number of approvals: count
-			review_count = int(re.search('"required_approving_review_count": \d', rules_dump).group()[-1])
-			result['require_pull_request_review'] = {}
-			result['require_pull_request_review']['enabled'] = True
-			result['require_pull_request_review']['approving_reviews_count'] = review_count
-			result['require_pull_request_review']['dismiss_stale_pull_request'] = dismiss_stale
-			result['require_pull_request_review']['require_review_from_code_owners'] = require_codeowners
-		except Exception:
-			result['require_pull_request_review'] = False
+			#TODO: We try the rules_dump becasue
+			try:
+				rules_dump = json.dumps(rules.json())
+				dismiss_stale = bool(re.search('"dismiss_stale_reviews": true', rules_dump))
+				require_codeowners = bool(re.search('"require_code_owner_reviews": true', rules_dump))
+				# get the minimum number of approvals: count
+				review_count = int(re.search('"required_approving_review_count": \d', rules_dump).group()[-1])
+				result['require_pull_request_review'] = {}
+				result['require_pull_request_review']['enabled'] = True
+				result['require_pull_request_review']['approving_reviews_count'] = review_count
+				result['require_pull_request_review']['dismiss_stale_pull_request'] = dismiss_stale
+				result['require_pull_request_review']['require_review_from_code_owners'] = require_codeowners
+			except Exception:
+				result['require_pull_request_review'] = False
 
 
-	#TODO: DOC
-	endpoint = f"{user}/{repo}/branches/{branch_name}"
-	branch_info = get_request(endpoint)
+		#TODO: DOC
+		endpoint = f"{user}/{repo}/branches/{branch_name}"
+		branch_info = get_request(endpoint)
 
-	if(branch_info.ok):
-		branch_load = json.loads(branch_info.content)
-		status_prot = branch_load["protection"]["required_status_checks"]["enforcement_level"]
-		if(status_prot == "off"):
-			result['require_status_checks'] = False
-		else:
-			branch = get_branch(g, user, repo, branch_name)
-			check_strict = bool(re.search("strict=True",str(branch.get_required_status_checks())))
-			result['require_status_checks'] = {}
-			result['require_status_checks']['enabled'] = True
-			result['require_status_checks']['strict'] = check_strict
+		if(branch_info.ok):
+			branch_load = json.loads(branch_info.content)
+			status_prot = branch_load["protection"]["required_status_checks"]["enforcement_level"]
+			if(status_prot == "off"):
+				result['require_status_checks'] = False
+			else:
+				branch = get_branch(g, user, repo, branch_name)
+				check_strict = bool(re.search("strict=True",str(branch.get_required_status_checks())))
+				result['require_status_checks'] = {}
+				result['require_status_checks']['enabled'] = True
+				result['require_status_checks']['strict'] = check_strict
 
-	#TODO: Get other rules using a new API call, why?
-	# Get additional rules
-	branch_info = get_branch(g, user, repo, branch_name)
-	result['require_signed_commits'] = branch_info.get_required_signatures()
-	result['include_administrators'] = branch_info.get_admin_enforcement()
+		#TODO: Get other rules using a new API call, why?
+		# Get additional rules
+		branch_info = get_branch(g, user, repo, branch_name)
+		result['require_signed_commits'] = branch_info.get_required_signatures()
+		result['include_administrators'] = branch_info.get_admin_enforcement()
 
-	return result
+		return result
+	except Exception:
+		return False
 
 
 if __name__ == '__main__':
@@ -151,7 +154,7 @@ if __name__ == '__main__':
 
 	# Get Branch Protection Rules
 	rules = get_branch_protection_rules(REST, USER, REPO, BRANCH)
-	print(rules)
+	# print(rules)
 
 	# Form the CRP
 	crp = form_crp(REST, USER, REPO, BRANCH)
