00ec1eb862e9e2d0cabe45445016f04427dcaf38
Hammad Afzali <hammad.afzali@gmail.com>
2021-03-10 17:42:02 -0500

    ENH: Add get_pr_code_changes

--- src/constants.py
+++ src/constants.py
@@ -1,38 +1,39 @@
 # GitHub
-CODEOWNERS = ".github/CODEOWNERS"
-GITATTRIBUTES = ".gitattributes"
-PROTECTION_RULES = "ProtectionRules"
-GITHUB_API = "https://api.github.com/repos"
 GITHUB = 'github'
-
+GITHUB_API = "https://api.github.com/repos"
 GITHUB_REQURIED_REVIEWS = 'required_pull_request_reviews'
 GITHUB_MIN_APPROALS = 'required_approving_review_count'
 GITHUB_DISMISS_STALE_REVIEWS = 'dismiss_stale_reviews'
 GITHUB_CODE_OWNER_REVIEWS = 'require_code_owner_reviews'
 GITHUB_ENFORCE_ADMIN = 'enforce_admins'
-
+CODEOWNERS = ".github/CODEOWNERS"
+GITATTRIBUTES = ".gitattributes"
+PROTECTION_RULES = "ProtectionRules"
 
 # Gerrit
+GERRIT = 'gerrit'
 CONFIG_BRANCH = 'refs/meta/config'
 CONFIG_PROJECT = 'project.config'
 CONFIG_GROUPS = 'groups'
 CONFIG_RULES = 'rules.pl'
 ALL_PROJECTS = 'All-Projects'
-GERRIT = 'gerrit'
 
-# Git
-FIRSTCOMMIT = 'FirstCommit'
-DIRECTPUSH = 'DirectPush'
+# Merge Options
+#MERGE_METHOD = 'merge_method'
 MERGE = 'Merge'
 REBASE = 'Rebase'
 SQUASH = 'SquashAndMerge'
+FIRSTCOMMIT = 'FirstCommit'
+DIRECTPUSH = 'DirectPush'
+
+# PGP
 PGP_SIG = 'gpgsig'
 PGP_START = '-----BEGIN PGP SIGNATURE-----'
 PGP_END = '-----END PGP SIGNATURE-----'
 
+# Crypto
 # TODO: Make it work for any condition in particular
-# when there is no existing keys at ~/.gnupg/
-#'C:\\Users\\hye\\.gnupg'
+# when there is no existing keys at ~/.gnupg/ or 'C:\\Users\\user\\.gnupg'
 KEYS_DIR = '/home/hmd/.gnupg'
 ED25519_KEY = 'ed25519_pub_key.pem'
 
